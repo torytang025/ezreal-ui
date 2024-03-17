13dd4c1a6b3968a208d68818476fd27d9082c80c
torytang025 <torytang025@users.noreply.github.com>
2024-03-17 10:05:42 +0000

    deploy: 9c72147d8d171a4abbd70d3380fe09fae39d8164

--- assets/preview-102edb91.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./iframe-1e062255.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-b97d2c92.js").then(e=>e.D),["./DocsRenderer-NNNQARDV-b97d2c92.js","./iframe-1e062255.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./_getPrototype-c468d7b4.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-102edb91.js.map
